24c9bf7d41e09e4444d1940bde705277c61c7f91
Julian <cusco2891@gmail.com>
2023-12-30 23:27:03 -0500

    headless by default

--- pdf.js
+++ pdf.js
@@ -3,13 +3,15 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: true });
+
+  // headless by default
+  // const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://news.ycombinator.com', {
     waitUntil: 'networkidle2',
   });
-  // page.pdf() is currently supported only in headless mode.
-  // @see https://bugs.chromium.org/p/chromium/issues/detail?id=753118
+
   await page.pdf({
     path: 'hn.pdf',
     format: 'letter',
